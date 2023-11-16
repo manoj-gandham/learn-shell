05c59b5a49ccab294cbc6418f21f981050dd700b
manoj <gandhamanojkutty@gmail.com>
2023-11-16 16:23:31 +0530

    code updated

---
+++ movie_rating.sh
@@ -0,0 +1,13 @@
+input=$1
+
+
+if [ -z "$input" ]; then
+  echo input movie name is missing
+  exit
+fi
+
+percent=$(curl -s https://www.themoviedb.org/tv/input=$1 | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
+
+echo percent = $percent
+
+if [ -z]
