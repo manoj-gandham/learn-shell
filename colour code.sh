c403a0185e2c6e1a6d25e38996d91faeca4748b1
manoj531222 <129020030+manoj531222@users.noreply.github.com>
2023-11-17 18:46:32 +0530

    Create colour code.sh

---
+++ colour code.sh
@@ -0,0 +1,7 @@
+Syntax = echo -e "\e[COLmMESSAGE\e[0m"
+
+
+# -e = enable colour in echo command
+# \e enable a colour
+# COLm = Color Code - Red(31m), Green(32m), Yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
+# \e[0m = disable the colour
